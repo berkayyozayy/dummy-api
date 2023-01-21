067d2da80310aa44ebaae672b9c11888660b8ea4
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-21 22:07:08 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
-import "./App.css";
-import Post from "presentation/components/post/post.js";
+import { Routes, Route } from "react-router-dom";
+
+import Posts from "presentation/components/posts/posts.js";
+import Users from "presentation/components/users/users.js";
+
 function App() {
   return (
     <div className="App">
-      <Post
-        avatarUrl="https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"
-        altText="user-image"
-        name="Berkay"
-        lastname="Özay"
-        description="Study Case"
-        date="23232"
-        count="2321"
-        tags="afawefae"
-      />
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </div>
   );
 }
