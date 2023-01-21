58711c366c16e56c03daa2ecedb2cff530e4f751
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-21 19:36:37 +0300

    feat: create global styles

---
+++ src/presentation/styles/global/colors.styled.js
@@ -0,0 +1,5 @@
+export const base = "white";
+export const papayawhip = "#ffefd5";
+export const text = "#434449";
+export const indigo = "#3f51b5";
+export const indigoDarken10 = "#364495";
---
+++ src/presentation/styles/global/global.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import * as colors from "presentation/styles/global/colors.styled";
+
+const Text = styled.p({
+  fontSize: "1em",
+  color: `${colors.indigo}`,
+});
+
+export { Text };
