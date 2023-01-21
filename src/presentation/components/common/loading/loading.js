435e4926aba45a82910c9887dee44e9898ca8161
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-21 21:01:30 +0300

    feat: create loading spinner component

---
+++ src/presentation/components/common/loading/loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Spinner } from "./loading.styled";
+
+function LoadingSpinner() {
+  return <Spinner />;
+}
+
+export default LoadingSpinner;
---
+++ src/presentation/components/common/loading/loading.styled.js
@@ -0,0 +1,20 @@
+import styled, { keyframes } from "styled-components";
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const Spinner = styled.div`
+  border: 4px solid #f3f3f3;
+  border-top: 4px solid #3498db;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  top: 50%;
+  left: 50%;
+  position: absolute;
+  animation: ${spin} 2s linear infinite;
+`;
+
+export { Spinner };
