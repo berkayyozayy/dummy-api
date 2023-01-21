d1e80f40c37a2f1539c01e8fcca3302b68329dfe
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-21 20:01:03 +0300

    post component test in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import "./App.css";
-
+import Post from "presentation/components/post/post.js";
 function App() {
-  return <div className="App">hello decathlon</div>;
+  return (
+    <div className="App">
+      <Post
+        avatarUrl="https://i.picsum.photos/id/237/200/300.jpg?hmac=TmmQSbShHz9CdQm0NkEjx1Dyh_Y984R9LpNrpvH2D_U"
+        altText="user-image"
+        name="Berkay"
+        lastname="Özay"
+        description="Study Case"
+        date="23232"
+        count="2321"
+        tags="afawefae"
+      />
+    </div>
+  );
 }
 
 export default App;
