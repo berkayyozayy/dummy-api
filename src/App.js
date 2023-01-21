046f0ee4d8c86fa338b08b554eadd1c13a1a79a2
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-21 23:09:44 +0300

    display header

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from "react-router-dom";
 
 import Posts from "presentation/components/posts/posts.js";
 import Users from "presentation/components/users/users.js";
+import Header from "presentation/components/header/header.js";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Posts />} />
         <Route path="/users" element={<Users />} />
