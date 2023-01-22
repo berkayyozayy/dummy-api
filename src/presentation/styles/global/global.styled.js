77b1e2cca30ec28521107c4e2f1856f19bd90fbc
Berkay <berkay@Berkay-MacBook-Air.local>
2023-01-22 13:03:00 +0300

    fix: font size

--- src/presentation/styles/global/global.styled.js
+++ src/presentation/styles/global/global.styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import * as colors from "presentation/styles/global/colors.styled";
 
 const Text = styled.p({
-  fontSize: "1em",
   color: `${colors.indigo}`,
 });
 
